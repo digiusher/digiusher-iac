eace97b230e7cb0057709cd87d7f5a4ce344f20d
Gaganpreet <me@gaganpreet.in>
2025-04-09 22:59:43 +0200

    Filter subscription ids and power scheduler for Azure (#10)

--- azure/azure_configuration.tf
+++ azure/azure_configuration.tf
@@ -26,6 +26,13 @@ variable "tenant_id" {
   description = "The Tenant ID which should be used"
 }
 
+# Variable for explicit subscription IDs
+variable "target_subscription_ids" {
+  type        = list(string)
+  description = "List of specific subscription IDs to target. If empty, all subscriptions will be used."
+  default     = []
+}
+
 provider "azurerm" {
   features {}
   subscription_id = var.subscription_id
@@ -35,14 +42,16 @@ provider "azurerm" {
 # Data source for Azure AD client config
 data "azuread_client_config" "current" {}
 
-# Get all subscriptions in the tenant
-data "azurerm_subscriptions" "available" {
-  display_name_contains = "" # Empty string means all subscriptions
-}
+# Get all available subscriptions in the tenant
+data "azurerm_subscriptions" "available" {}
 
 locals {
-  # Extract subscription IDs from the data source
-  subscriptions = [for s in data.azurerm_subscriptions.available.subscriptions : s.subscription_id]
+  # Use explicit subscription IDs if provided, otherwise use all subscriptions
+  all_subscriptions = [
+    for s in data.azurerm_subscriptions.available.subscriptions : s.subscription_id
+  ]
+
+  subscriptions = length(var.target_subscription_ids) > 0 ? var.target_subscription_ids : local.all_subscriptions
 }
 
 # Create a single application for the tenant
@@ -64,12 +73,44 @@ resource "azuread_application_password" "app_password" {
   application_object_id = azuread_application.digiusher_app.object_id
 }
 
+variable "enable_power_scheduler" {
+  type        = bool
+  description = "If true, adds permissions to start and deallocate (stop) VMs"
+  default     = false
+}
+
+resource "azurerm_role_definition" "digiusher_power_scheduler" {
+  count       = var.enable_power_scheduler ? 1 : 0
+  name        = "DigiUsher Power Scheduler"
+  scope       = "/subscriptions/${var.subscription_id}"
+  description = "Custom role for managing VM start/deallocate actions"
+
+  permissions {
+    actions = [
+      "Microsoft.Compute/virtualMachines/start/action",
+      "Microsoft.Compute/virtualMachines/deallocate/action",
+      "Microsoft.Compute/virtualMachines/read"
+    ]
+    not_actions = []
+  }
+
+  assignable_scopes = [for sub_id in local.subscriptions : "/subscriptions/${sub_id}"]
+}
+
 # Assign Reader role to the service principal in each subscription
 resource "azurerm_role_assignment" "app_role_assignment" {
   count                = length(local.subscriptions)
   scope                = "/subscriptions/${local.subscriptions[count.index]}"
   role_definition_name = "Reader"
   principal_id         = azuread_service_principal.digiusher_app_sp.object_id
+}
+
+# Assign power scheduler role if enabled
+resource "azurerm_role_assignment" "digiusher_power_scheduler_role_assignment" {
+  count               = var.enable_power_scheduler ? length(local.subscriptions) : 0
+  scope               = "/subscriptions/${local.subscriptions[count.index]}"
+  role_definition_id  = azurerm_role_definition.digiusher_power_scheduler[0].role_definition_resource_id
+  principal_id        = azuread_service_principal.digiusher_app_sp.object_id
 }
 
 
